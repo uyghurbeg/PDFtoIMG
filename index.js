4cd81fd873f5b30c55f60b757caa7399c9bfc4e7
Uyghurbeg <uyghurbeg@outlook.com>
2020-03-03 01:20:49 +0100

    prepared to finih

--- index.js
+++ index.js
@@ -1,15 +1,26 @@
 const express = require('express')
 const app = express()
-const formidable = require('formidable')
+const { IncomingForm } = require('formidable');
 const bodyParser = require('body-parser')
 const fs = require('fs-extra')
+var PDFImage = require("pdf-image").PDFImage;
 
 app.get('/', (req, res) => {
-    res.sendFile(__dirname+ 'index.html');
+    res.sendFile(__dirname+ '/index.html');
+})
+
+app.get('/upload', (req, res) => {
+    res.sendFile(__dirname+ '/index.html');
 })
 
 app.post('/upload', (req, res) => {
-    
+    const form = new IncomingForm();
+ 
+    form.parse(req);
+     
+    form.on('file', (filename, file) => {
+        fs.copyFile(file.path, '/Users/mac/Downloads/PDFtoIMG/tmp')
+    });
 })
 
 app.listen(3000, (err) => {
